d7d1ee57cf79f68b9dd890c6a1a98342e49adc7e
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-09 15:12:17 +0530

    9/7/2024

---
+++ 1stpart_tricp.py
@@ -0,0 +1,12 @@
+data = {}
+print(type(data))
+# Remember floor division : making it equivalent to the math.floor() function
+# The result of regular division (using the / operator) is 15/4 = 3.75, but using // has floored 3.75 down to 3
+
+
+print(-10//3)
+print(11//3)
+
+# print(''conversation - He said, "Shantanu's explanation is very good" and I subscribed channel"")
+print("""conversation - He said, "Shree`s explanation is very good" and I subscribed to his blog""")
+print("\"""conversation - He said, \"Shree\'s explanation is very good\" and I subscribed to his blog\"""")
